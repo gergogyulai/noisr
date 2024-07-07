c8d1587d991eb22b6b14b29b86b1ebaed9e0927d
gergo <113334790+gergogyulai@users.noreply.github.com>
2024-07-07 16:57:43 +0200

    Update footer.tsx

--- components/footer.tsx
+++ components/footer.tsx
@@ -6,7 +6,7 @@ function Footer() {
     <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
       <a
         className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-        href="https://github.com/gergogyulai/noiser"
+        href="https://github.com/gergogyulai/noisr"
         target="_blank"
       >
         <Github size={16} />
